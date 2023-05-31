229bf44ad3e717ccb8d7b09d9d186e2397a5250e
Uros Rajkovic <urosrajkovic011@hotmail.com>
2023-05-31 22:32:21 +0200

    factory and seed

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Listing
+class Listing extends Model
 {
-    public static  function all(){
-        return [[
-            'id' => 1,
-            'title' => 'Listing one',
-            'description' => 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia'
-        ],
-            [
-                'id' => 2,
-                'title' => 'Listing two',
-                'description' => 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia'
-            ]
-        ];
-    }
-    public static function find($id){
+    use HasFactory;
 
-        $listings = self::all();
 
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
 }
